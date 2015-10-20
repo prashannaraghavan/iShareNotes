1cb7702278b0e12be11f2971fa72392ae07b853f
Prashanna Raghavan <Pras@prashannas-mac.local>
2015-10-20 00:07:50 -0700

    Initial Commit

---
+++ iShareNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iShareNotes
+//
+//  Created by Prashanna Raghavan on 10/20/15.
+//  Copyright © 2015 Prashanna Raghavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iShareNotesTests/iShareNotesTests.m
@@ -0,0 +1,39 @@
+//
+//  iShareNotesTests.m
+//  iShareNotesTests
+//
+//  Created by Prashanna Raghavan on 10/20/15.
+//  Copyright © 2015 Prashanna Raghavan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iShareNotesTests : XCTestCase
+
+@end
+
+@implementation iShareNotesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
